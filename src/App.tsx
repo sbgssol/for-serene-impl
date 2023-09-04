b17017fa362694d6322beead8ab845c068848a2e
Ly Nguyen <nhly.se@gmail.com>
2023-09-04 11:22:42 +0700

    add deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -6,202 +6,202 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function App() {
-    // const navigate = useNavigate();
-    const handlePrevClick = () => {
-        alert("Prev clicked!");
-    };
+  // const navigate = useNavigate();
+  const handlePrevClick = () => {
+    alert("Prev clicked!");
+  };
 
-    const [flyIn1, setFlyIn1] = useState(false);
-    const [flyIn2, setFlyIn2] = useState(false);
-    const [flyIn3, setFlyIn3] = useState(false);
-    const [flyIn4, setFlyIn4] = useState(false);
-    const [flyIn5, setFlyIn5] = useState(false);
-    const navigate = useNavigate();
+  const [flyIn1, setFlyIn1] = useState(true);
+  const [flyIn2, setFlyIn2] = useState(false);
+  const [flyIn3, setFlyIn3] = useState(false);
+  const [flyIn4, setFlyIn4] = useState(false);
+  const [flyIn5, setFlyIn5] = useState(false);
+  const navigate = useNavigate();
 
-    const handleNextClick = () => {
-        if (!flyIn1) {
-            setFlyIn1(!flyIn1);
-            return;
-        }
-        if (!flyIn2) {
-            setFlyIn2(!flyIn2);
-            return;
-        }
-        if (!flyIn3) {
-            setFlyIn3(!flyIn3);
-            return;
-        }
-        if (!flyIn4) {
-            setFlyIn4(!flyIn4);
-            return;
-        }
-        if (!flyIn5) {
-            setFlyIn5(!flyIn5);
-            return;
-        }
-        navigate("/for-serene-impl/2");
-    };
+  const handleNextClick = () => {
+    if (!flyIn1) {
+      setFlyIn1(!flyIn1);
+      return;
+    }
+    if (!flyIn2) {
+      setFlyIn2(!flyIn2);
+      return;
+    }
+    if (!flyIn3) {
+      setFlyIn3(!flyIn3);
+      return;
+    }
+    if (!flyIn4) {
+      setFlyIn4(!flyIn4);
+      return;
+    }
+    if (!flyIn5) {
+      setFlyIn5(!flyIn5);
+      return;
+    }
+    navigate("/for-serene-impl/2");
+  };
 
-    const genThanks = (msg: string) => {
-        if (!flyIn1) {
-            return (
-                <Typography
-                    variant="h2"
-                    className="font-signika text-pink-100 pr-3 opacity-0"
-                >
-                    {msg}
-                </Typography>
-            );
-        } else {
-            return (
-                <Typography
-                    variant="h2"
-                    className="font-signika text-pink-100 pr-3 animate-flyLeft"
-                >
-                    {msg}
-                </Typography>
-            );
-        }
-    };
+  const genThanks = (msg: string) => {
+    if (!flyIn1) {
+      return (
+        <Typography
+          variant="h2"
+          className="font-signika text-pink-100 pr-3 opacity-0"
+        >
+          {msg}
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography
+          variant="h2"
+          className="font-signika text-pink-100 pr-3 animate-flyLeft"
+        >
+          {msg}
+        </Typography>
+      );
+    }
+  };
 
-    const genHeader = (msg: string) => {
-        if (flyIn2) {
-            return (
-                <>
-                    <Typography
-                        variant="h2"
-                        className={`font-marmelad text-pink-300 bg-pink-50 text-6xl px-3 py-1 box-content rounded-md uppercase animate-flyRight`}
-                    >
-                        {msg}
-                    </Typography>
-                </>
-            );
-        } else {
-            return (
-                <>
-                    <Typography
-                        variant="h2"
-                        className={`font-marmelad text-pink-300 bg-pink-50 text-6xl px-3 py-1 box-content rounded-md uppercase opacity-0`}
-                    >
-                        {msg}
-                    </Typography>
-                </>
-            );
-        }
-    };
-    const genHead = (msg: string) => {
-        if (flyIn5) {
-            return (
-                <>
-                    <Typography
-                        variant="h2"
-                        className={`font-marmelad text-pink-50 bg-pink-300 text-6xl px-3 py-1 box-content rounded-md uppercase animate-flyUp`}
-                    >
-                        {msg}
-                    </Typography>
-                </>
-            );
-        } else {
-            return (
-                <>
-                    <Typography
-                        variant="h2"
-                        className={`font-marmelad text-pink-50 bg-pink-300 text-6xl px-3 py-1 box-content rounded-md uppercase opacity-0`}
-                    >
-                        {msg}
-                    </Typography>
-                </>
-            );
-        }
-    };
-
-    const genMine = (msg: string) => {
-        if (!flyIn3) {
-            return (
-                <Typography
-                    variant="h2"
-                    className="font-signika text-pink-300 opacity-0"
-                >
-                    {msg}
-                </Typography>
-            );
-        } else {
-            return (
-                <Typography
-                    variant="h2"
-                    className="font-signika text-pink-300 animate-flyRight"
-                >
-                    {msg}
-                </Typography>
-            );
-        }
-    };
-
-    const genBecause = (msg: string) => {
-        if (!flyIn4) {
-            return (
-                <Typography
-                    variant="h3"
-                    className="font-signika text-pink-100 opacity-0"
-                >
-                    {msg}
-                </Typography>
-            );
-        } else {
-            return (
-                <Typography
-                    variant="h3"
-                    className="font-signika text-pink-100 animate-flyRight "
-                >
-                    {msg}
-                </Typography>
-            );
-        }
-    };
-
-    // const genHere = (msg: string) => {
-
-    // };
-
-    return (
+  const genHeader = (msg: string) => {
+    if (flyIn2) {
+      return (
         <>
-            <div className="flex flex-col items-center">
-                <div className="container flex items-center justify-center bg-[#333] w-[800px] h-[600px] mt-9 rounded-xl shadow-md shadow-pink-800/70">
-                    <div className="flex flex-col items-center">
-                        <div className="header flex justify-center mt-10 place-items-baseline">
-                            {genThanks("Cảm ơn em,")}
-                            <div className="sub-header flex flex-col items-start">
-                                <div className="flex">
-                                    {genHeader("Công chúa")}
-                                </div>
-                                {genMine("của anh")}
-                                <div className="flex justify-between w-full">
-                                    <div className="text-center">{genBecause("vì em còn ")}</div>
-                                    <div className="w-full text-center">{genHead("Ở đây ❤️")}</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex place-content-between w-80 md:w-[720px] lg:w-[800px] mt-5">
-                    <Button
-                        className="p-0 bg-transparent shadow-none hover:shadow-none transition-transform hover:scale-150 duration-150"
-                        disabled
-                        onClick={handlePrevClick}
-                    >
-                        <img src={LeftIcon} alt="" />
-                    </Button>
-                    <Button
-                        className="p-0 bg-transparent shadow-none hover:shadow-none transition-transform hover:scale-150 duration-150"
-                        onClick={handleNextClick}
-                    >
-                        <img src={RightIcon} alt="" />
-                    </Button>
-                </div>
-            </div>
+          <Typography
+            variant="h2"
+            className={`font-marmelad text-pink-300 bg-pink-50 text-6xl px-3 py-1 box-content rounded-md uppercase animate-flyRight`}
+          >
+            {msg}
+          </Typography>
         </>
-    );
+      );
+    } else {
+      return (
+        <>
+          <Typography
+            variant="h2"
+            className={`font-marmelad text-pink-300 bg-pink-50 text-6xl px-3 py-1 box-content rounded-md uppercase opacity-0`}
+          >
+            {msg}
+          </Typography>
+        </>
+      );
+    }
+  };
+  const genHead = (msg: string) => {
+    if (flyIn5) {
+      return (
+        <>
+          <Typography
+            variant="h2"
+            className={`font-marmelad text-pink-50 bg-pink-300 text-6xl px-3 py-1 box-content rounded-md uppercase animate-flyUp`}
+          >
+            {msg}
+          </Typography>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Typography
+            variant="h2"
+            className={`font-marmelad text-pink-50 bg-pink-300 text-6xl px-3 py-1 box-content rounded-md uppercase opacity-0`}
+          >
+            {msg}
+          </Typography>
+        </>
+      );
+    }
+  };
+
+  const genMine = (msg: string) => {
+    if (!flyIn3) {
+      return (
+        <Typography
+          variant="h2"
+          className="font-signika text-pink-300 opacity-0"
+        >
+          {msg}
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography
+          variant="h2"
+          className="font-signika text-pink-300 animate-flyRight"
+        >
+          {msg}
+        </Typography>
+      );
+    }
+  };
+
+  const genBecause = (msg: string) => {
+    if (!flyIn4) {
+      return (
+        <Typography
+          variant="h3"
+          className="font-signika text-pink-100 opacity-0"
+        >
+          {msg}
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography
+          variant="h3"
+          className="font-signika text-pink-100 animate-flyRight "
+        >
+          {msg}
+        </Typography>
+      );
+    }
+  };
+
+  // const genHere = (msg: string) => {
+
+  // };
+
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <div className="container flex items-center justify-center bg-[#333] w-[800px] h-[600px] mt-9 rounded-xl shadow-md shadow-pink-800/70">
+          <div className="flex flex-col items-center">
+            <div className="header flex justify-center mt-10 place-items-baseline">
+              {genThanks("Cảm ơn em,")}
+              <div className="sub-header flex flex-col items-start">
+                <div className="flex">{genHeader("Công chúa")}</div>
+                {genMine("của anh")}
+                <div className="flex justify-between w-full">
+                  <div className="text-center">{genBecause("vì em còn ")}</div>
+                  <div className="w-full text-center">
+                    {genHead("Ở đây ❤️")}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex place-content-between w-80 md:w-[720px] lg:w-[800px] mt-5">
+          <Button
+            className="p-0 bg-transparent shadow-none hover:shadow-none transition-transform hover:scale-150 duration-150"
+            disabled
+            onClick={handlePrevClick}
+          >
+            <img src={LeftIcon} alt="" />
+          </Button>
+          <Button
+            className="p-0 bg-transparent shadow-none hover:shadow-none transition-transform hover:scale-150 duration-150"
+            onClick={handleNextClick}
+          >
+            <img src={RightIcon} alt="" />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Page/Auth/Authorization.tsx
+++ src/components/Page/Auth/Authorization.tsx
@@ -2,63 +2,65 @@ import { Button, Input, Typography } from "@material-tailwind/react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 interface AuthorizationProps {
-    setValidForRouter: (valid: boolean) => void; // Define a prop to set the valid state in MyRouter
+  setValidForRouter: (valid: boolean) => void; // Define a prop to set the valid state in MyRouter
 }
 
-export default function Authorization({ setValidForRouter }: AuthorizationProps) {
-    const [input, setInput] = useState<string>("");
-    const [valid, setValid] = useState(false);
-    const navigate = useNavigate();
+export default function Authorization({
+  setValidForRouter,
+}: AuthorizationProps) {
+  const [input, setInput] = useState<string>("");
+  const [valid, setValid] = useState(false);
+  const navigate = useNavigate();
 
-    const handleChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(event.target.value);
-    };
+  const handleChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
 
-    const checkValid = () => {
-        setValid(input.toLowerCase() === "cck" || input.toLowerCase() === "circle k"); // Update the valid state in MyRouter
-        setValidForRouter(valid);
-        return valid;
-    };
-
-    const handleContinueClicked = () => {
-        if (!checkValid()) return;
-        navigate("/for-serene-impl/1");
-    };
-
-    useEffect(() => {
-        checkValid();
-        // console.log("input: " + input);
-    }, [input]);
-
-    return (
-        <div className="w-screen flex justify-center mt-6">
-            <div className="w-[32rem] ">
-                <Typography
-                    variant="lead"
-                    color="pink"
-                    className="flex items-center gap-1 font-normal mb-2 font-signika"
-                >
-                    Where is the place that I had a first kiss on your lips?
-                </Typography>
-                <Input
-                    crossOrigin={undefined}
-                    type="password"
-                    color="pink"
-                    value={input}
-                    onChange={handleChanged}
-                    label={valid ? "Oh Yeah ❤️" : "Do you remember?"}
-                />
-                <Button
-                    ripple={valid ? true : false}
-                    color={valid ? "pink" : "gray"}
-                    className={`mt-2 w-full ${
-                        valid ? "" : "hover:cursor-not-allowed"
-                    }`}
-                    onClick={handleContinueClicked}
-                >
-                    Continue →
-                </Button>
-            </div>
-        </div>
+  const checkValid = () => {
+    setValid(
+      input.toLowerCase() === "cck" || input.toLowerCase() === "circle k"
     );
+    setValidForRouter(valid);
+    return valid;
+  };
+
+  const handleContinueClicked = () => {
+    if (!checkValid()) return;
+    navigate("/for-serene-impl/1");
+  };
+
+  useEffect(() => {
+    checkValid();
+    return () => {};
+  }, [input]);
+
+  return (
+    <div className="w-screen flex justify-center mt-6">
+      <div className="w-[32rem] ">
+        <Typography
+          variant="lead"
+          color="pink"
+          className="flex items-center gap-1 font-normal mb-2 font-signika"
+        >
+          Where is the place that I had a first kiss on your lips?
+        </Typography>
+        <Input
+          crossOrigin={undefined}
+          type="password"
+          color="pink"
+          value={input}
+          onChange={handleChanged}
+          label={valid ? "Oh Yeah ❤️" : "Do you remember?"}
+        />
+        <Button
+          ripple={valid ? true : false}
+          color={valid ? "pink" : "gray"}
+          className={`mt-2 w-full ${valid ? "" : "hover:cursor-not-allowed"}`}
+          onClick={handleContinueClicked}
+        >
+          Continue →
+        </Button>
+      </div>
+    </div>
+  );
 }
