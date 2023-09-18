ec229b4e4b84b0e944d2226a917585bcc9161f5e
Ly Nguyen <nhly.se@gmail.com>
2023-09-18 22:26:28 +0700

    add deploy workflow

--- src/components/Page/Normal/Ask.tsx
+++ src/components/Page/Normal/Ask.tsx
@@ -17,39 +17,46 @@ export default function Ask(props: AskProps) {
   return (
     <>
       <Dialog
-        size="xs"
+        size="sm"
         open={props.show}
         handler={props.handler}
-        className="bg-transparent"
+        className="bg-transparent "
       >
-        <DialogHeader className="font-primary rounded-t-xl bg-gray-800 text-primaryPink-400 justify-center py-2">
-          Cho anh một cơ hội, em nhé
+        <DialogHeader
+          className="font-primary rounded-t-xl bg-gray-800 
+          text-primaryPink-400 justify-center py-2 
+          border-t-2 border-t-primaryPink-600
+          border-r-2 border-r-primaryPink-600
+          border-l-2 border-l-primaryPink-600"
+        >
+          Cho anh một cơ hội, em nhớ
         </DialogHeader>
         <DialogBody
           divider
-          className="bg-gray-800 text-primaryPink-300 px-2 py-2"
+          className="bg-gray-800 text-primaryPink-300 px-2 py-2
+          border-r-2 border-r-primaryPink-600
+          border-l-2 border-l-primaryPink-600"
         >
-          <Typography className="font-primary text-lg font-normal">
-            Anh sẽ yêu em thật nhiều.
+          <Typography className="font-secondary text-lg font-normal">
+            Nếu phải lựa chọn giữa việc yêu em và oxy để thở,
           </Typography>
-          <Typography className="font-primary text-lg font-normal">
-            Anh sẽ luôn tôn trọng em.
-          </Typography>
-          <Typography className="font-primary text-lg font-normal">
-            Anh sẽ luôn đối xử tốt với em.
-          </Typography>
-          <Typography className="font-primary text-lg font-normal">
-            Cho anh cơ một lần này, em nhé.
+          <Typography className="font-secondary text-lg font-normal">
+            Anh sẽ dùng hơi thở cuối cùng để nói lời yêu em.
           </Typography>
         </DialogBody>
-        <DialogFooter className="bg-gray-800 rounded-b-xl py-2 justify-center">
+        <DialogFooter
+          className="bg-gray-800 rounded-b-xl py-2 justify-center
+        border-b-2 border-b-primaryPink-600
+        border-r-2 border-r-primaryPink-600
+        border-l-2 border-l-primaryPink-600"
+        >
           <Button
             variant="gradient"
             color="pink"
             onClick={props.okayHandler}
-            className="font-primary wide normal-case text-sm font-normal"
+            className="font-primary wide normal-case text-sm font-normal border-2 border-gray-200"
           >
-            <span>Rep anh nhá</span>
+            <span>Rep anh nha</span>
           </Button>
         </DialogFooter>
       </Dialog>
--- src/components/Page/Normal/WordsMiss.tsx
+++ src/components/Page/Normal/WordsMiss.tsx
@@ -15,6 +15,7 @@ export default function WordsMiss() {
     "Anh nhớ giọng nói của em, nhớ tiêng cười, và cả tiếng em khóc nấc lên biết bao lần vì anh.",
     "Anh nhớ đôi gò má của em, hồng hồng khi anh bất ngờ trao em một chiếc hôn, hay phồng lên khi anh khiến em phải buồn.",
     "Anh nhớ đôi tay của em, đôi bàn tay vụng về hậu đậu, luôn làm đỗ vỡ khi làm việc nhà, nhưng lại siết thật chặt mỗi khi cầm lấy tay anh.",
+    "Anh sẽ giữ trọn hình bóng em trong trái tim anh. Nụ cười rạng rỡ, khuôn mặt dịu dàng luôn nhìn anh bằng ánh mắt ngập tràn tình yêu.",
     "Em, tất cả những điều thuộc về em, đã cắm rễ trong trái tim anh, âm thầm, nhẹ nhàng, lặng lẽ, những ngày càng đậm sâu, và vững chắc.",
   ];
 
